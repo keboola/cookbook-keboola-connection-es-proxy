6476f9a46dc440f5197c2d90c7569b9d14d21790
Martin Halamíček <martin@halamicek.cz>
2015-09-30 12:18:14 +0200

    init

---
+++ attributes/default.rb
@@ -0,0 +1,10 @@
+
+
+default['keboola-connection-es-proxy']['password'] = ''
+
+default['apache']['default_modules'] = %w[
+  dir env mime negotiation setenvif
+  status alias auth_basic authn_core authn_file authz_core authz_groupfile
+  authz_host authz_user autoindex dir env mime negotiation setenvif logio log_config unixd socache_shmcb filter
+  proxy proxy_http
+]
---
+++ metadata.rb
@@ -0,0 +1,13 @@
+name             'keboola-connection-es-proxy'
+maintainer       'Keboola'
+maintainer_email 'martin@keboola.com'
+license          'All rights reserved'
+description      'Installs/Configures keboola-generic-server'
+version          '0.0.1'
+
+
+depends 'aws', '~> 2.4.0'
+depends 'keboola-php56'
+depends 'keboola-common'
+depends 'keboola-apache2'
+depends 'htpasswd', '~> 0.2.4'
